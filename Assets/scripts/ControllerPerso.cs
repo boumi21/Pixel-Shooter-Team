d2b66ab07069db4752860a1780e44fa1de0347f0
Bassirou1 <haidara.bassirou@yahoo.com>
2017-12-02 15:59:10 -0500

    Perso

--- Assets/scripts/ControllerPerso.cs
+++ Assets/scripts/ControllerPerso.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class ControllerPerso : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public static float vitesse = 5f;
+	private Rigidbody body;
+	//private List<Noeud> pathPoint;
+	public GameObject controller;
+
+	void Start () 
+	{
+		body = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	/*void FixedUpdate () 
+	{
+		pathPoint = controller.GetComponent<Grille>().chemin;
+		Vector3 target = pathPoint[0].position;
+		Vector3 moveDirection = target - this.transform.position;
+		Vector3 velocity = body.velocity;
+
+		print(moveDirection.ToString());
+
+		if (moveDirection.magnitude < .1)
+			velocity = new Vector3(0f, 0f, 0f);
+		else
+			velocity = moveDirection.normalized * vitesse;
+		body.velocity = velocity;
+	}*/
 }
