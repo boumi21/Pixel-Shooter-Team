ad442a898232bfbb0a21f2192ab85d3578fdd83c
ericPellletier <pelletierric@gmail.com>
2017-12-02 13:49:59 -0500

    AI ennemi + script
    Ennemi sript +

---
+++ Assets/scripts/ControleProjectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleProjectile : MonoBehaviour
+{
+    public LayerMask cible;
+    // Use this for initialization
+    void Start()
+    {
+        // Destroy(this.gameObject, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.gameObject.tag == "Ennemi")
+        {
+            obj.SendMessage("perdreVie");
+            Destroy(this.gameObject);
+        }
+
+
+
+    }
+}
---
+++ Assets/scripts/VieEnnemi.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VieEnnemi : MonoBehaviour
+{
+    public int vie = 1;
+    // Use this for initialization
+    void Start()
+    {
+        if (this.gameObject.tag == "Dragon")
+            this.vie += 3;
+        else if (this.gameObject.tag == "LimaceVerte")
+            this.vie++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vie <= 0)
+            Destroy(this.gameObject);
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "BouleFeu")
+            perdreVie();
+
+        Destroy(this);
+    }
+    public void perdreVie()
+    {
+        vie--;
+    }
+
+}
