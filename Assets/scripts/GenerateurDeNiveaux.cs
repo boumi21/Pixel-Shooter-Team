03056e68ea5a6bcddffad260aff9a904b23c2731
gui45 <guillaume.d.marceau@gmail.com>
2017-12-02 11:30:17 -0500

    debut generation

--- Assets/scripts/GenerateurDeNiveaux.cs
+++ Assets/scripts/GenerateurDeNiveaux.cs
@@ -1,16 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
 
 public class GenerateurDeNiveaux : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	private byte hauteur = 20;
+	private byte longueur = 30;
+	private int quantiterBlockers = 200;
+	private int quantiterDecorations = 300;
+	private List<Vector3> positions = new List<Vector3>();
+	public GameObject carte;
+
+	public GameObject[] blockers;
+	public GameObject[] decorations;
+	public GameObject grass;
+
+	void Start ()
+	{
+		carte = new GameObject ("carte");
+		for (byte x = longueur; x > 0; x--)
+			for (byte y = hauteur; y > 0; y--) 
+				positions.Add (new Vector3 (x, y, 0f));
+			
 		
+		generation ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	private void generation()
+	{
+		placerSol ();
+	}
+	private void placerSol()
+	{
+		foreach (Vector3 place in positions) 
+		{
+			GameObject instance = Instantiate (grass, place, Quaternion.identity);
+			instance.transform.SetParent (carte.transform);
+		}
 	}
 }
