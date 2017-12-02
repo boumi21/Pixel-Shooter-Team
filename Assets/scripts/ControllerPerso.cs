0e2c2ddb6283a13cc4bab5107ef117caeab63c9a
Bassirou1 <haidara.bassirou@yahoo.com>
2017-12-02 14:23:39 -0500

    animationPerso

--- Assets/scripts/ControllerPerso.cs
+++ Assets/scripts/ControllerPerso.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControllerPersonnage : MonoBehaviour {
+public class ControllerPerso : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
